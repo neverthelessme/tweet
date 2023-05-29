b8b26e8a344e79a33e3542cd8db5a704486b5b09
neverthelessme <122379541+neverthelessme@users.noreply.github.com>
2023-05-29 13:31:02 +0530

    Update tweet.scala

--- tweet.scala
+++ tweet.scala
@@ -71,3 +71,11 @@ object TweetUtils {
 		lines.map(line => gson.fromJson(line, classOf[Tweet]))
 	}
 }
+
+
+
+
+name:="tweet"
+version:="1.0"
+scalaVersion:="2.11.12"
+libraryDependencies+="org.apache.spark"%%"spark-core"%"2.3.0"
